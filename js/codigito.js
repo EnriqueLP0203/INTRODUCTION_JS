1395c9be96c321982b56f207820f390c3bcd34dc
EnriqueLP0203 <2302045@utrivieramaya.edu.mx>
2024-10-23 09:18:13 -0500

    first commit

---
+++ js/codigito.js
@@ -0,0 +1,10 @@
+/*los comentarios pueden hacer que el codigo sea mas legible*/
+
+/*Este es mi primer codigo JS*/
+
+/*Los comentarios pueden
+hacer que el codigo
+sea mas legible, facil de 
+reutilizar e informativo*/
+
+
---
+++ js/variable.js
@@ -0,0 +1,66 @@
+/* cadena */
+
+let nombre="Enrique";
+
+let saludo = "Hola bienvenido a mi codigo";
+
+/* booleano */
+
+let NumMayor = true;
+let NumMenor = false;
+
+/* indefinido */
+
+let nombre1;
+let edad;
+
+/* nulo */
+
+let laptop = null
+let cellphone = null
+
+
+
+
+//declara 4 variables sin declarar datos
+
+let dia1;
+let dia2;
+let dia3;
+let dia4;
+
+
+//decclara 4 variables con valores asignados
+
+let persona1 = 'Juan';
+let persona2 = 'Luis';
+let persona3 = 'Rodrigo';
+let persona4 = 'Julian';
+
+
+
+
+//Declare variables para almacenar su nombre, apellido, estado civil, país y edad en varias lineas
+
+let myName = 'Enrique';
+let myLastname = 'Castillo';
+let isMarried = false;
+let Country= 'Mexico';
+let age = 21;
+
+
+
+let myName1, myLastname1, isMarried1, Country1, age1;
+
+name1='topo';
+name1='carlo';
+
+
+//Declare dos variables myAge y yourAge, asígneles valores iniciales e inicie sesión en la consolo de navegador
+
+
+let myAge = 21;
+let yourAge = 25;
+
+
+console.log('My age is ' + myAge, ', tu edad es ' + yourAge);
